6eb0a49d681ebadf21fb108634cfa42080a08f85
kite <sarang.thekkedathpr@gmail.com>
2021-08-06 08:09:06 +0530

    backup

---
+++ script.js
@@ -0,0 +1,8 @@
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+function aboutme() {
+    document.body.scrollTop = 100; // For Safari
+    document.documentElement.scrollTop = 100; // For Chrome, Firefox, IE and Opera
+}
